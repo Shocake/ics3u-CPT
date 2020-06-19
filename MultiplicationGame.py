5f843cca6ba803c5c60e391204562ea14673a2dc
Joseph Liu <josephliu@Josephs-MacBook-Air.local>
2020-06-19 19:09:25 -0400

    added file

---
+++ MultiplicationGame.py
@@ -0,0 +1,22 @@
+import random
+Number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+a = str(random.choice(Number))
+b = str(random.choice(Number))
+Answer = input("What is " +  a + "x" + b + ":")
+Answer = int(Answer)
+a = int(a)
+b = int(b)
+Points = 0
+while int(Answer) > 0:
+    if int(Answer) == (int(a) * int(b)):
+        Points += 1
+        print("You're right! Points:" + str(Points))
+        a = str(random.choice(Number))
+        b = str(random.choice(Number))
+        Answer = input("What is " +  a + "x" + b + ":")
+        int(a)
+        int(b)
+    else:
+        print("Wrong. Game Over!")
+        break
+
